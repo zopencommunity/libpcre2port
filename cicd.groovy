b00041b3efc291c68721af01b131814fb831eee9
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:22 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libpcre2port.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libpcre2port.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libpcre2port.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl Compatible Regular Expressions' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libpcre2port.git'), string(name: 'PORT_DESCRIPTION', value: 'Perl Compatible Regular Expressions' )]
   }
 }
